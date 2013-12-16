06a3a32c6c6cdea5adeaca9b2ebf99feb40eff72
Camael24 <thehawk@hoa-project.net>
2013-12-16 10:10:43 +0100

    Add Validate

---
+++ Sources/Hawk/Validate/Validator/Strlen.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julien
+ * Date: 13/12/13
+ * Time: 10:30
+ */
+namespace Hawk\Validate\Validator{
+    class Strlen extends String
+    {
+        protected $_function = 'strlen';
+
+    }
+}
+ 
